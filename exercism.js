ed603af53264ffad64c0a831fde64c590d1d0da6
Abdur R. Walid <ar_olid@outlook.com>
2022-08-22 20:13:08 -0600

    daily exercise

--- exercism.js
+++ exercism.js
@@ -1,3 +1,5 @@
+//Excercise 1 : Basics
+
 export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 export function remainingMinutesInOven(min) {
@@ -17,3 +19,69 @@ export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
   return layerTime + actualMinutesInOven;
 }
 totalTimeInMinutes(1, 5);
+
+// Excercise 2 : Numbers
+
+export function dayRate(ratePerHour) {
+  let dailyRate = ratePerHour * 8;
+  return dailyRate;
+}
+dayRate(3);
+
+export function daysInBudget(budget, ratePerHour) {
+  let numberOfDays = Math.floor(budget / ratePerHour / 8);
+  return numberOfDays;
+}
+daysInBudget(2000, 89);
+
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const fullMonths = Math.floor(numDays / 22);
+  const remainingDays = numDays % 22;
+  const fullMonthPrice = fullMonths * 22 * dayRate(ratePerHour);
+  return Math.ceil(
+    fullMonthPrice -
+      discount * fullMonthPrice +
+      remainingDays * dayRate(ratePerHour)
+  );
+}
+priceWithMonthlyDiscount(33, 778, 0);
+
+//Exercise: 3
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  if (knightIsAwake || archerIsAwake || prisonerIsAwake) {
+    return true;
+  } else {
+    return false;
+  }
+}
+canSpy(true, true, false);
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  if (!archerIsAwake && prisonerIsAwake) {
+    return true;
+  } else {
+    return false;
+  }
+}
+canSignalPrisoner(true, false);
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  if (petDogIsPresent && !archerIsAwake) {
+    return true;
+  } else if (
+    prisonerIsAwake &&
+    !petDogIsPresent &&
+    !knightIsAwake &&
+    !archerIsAwake
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+canFreePrisoner(true, false, true, true);
