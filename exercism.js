16f787a05584c7e12af346c95b1a27ac0c5c1482
Abdur R. Walid <ar_olid@outlook.com>
2022-08-24 00:52:03 -0600

    daily exercise

--- exercism.js
+++ exercism.js
@@ -172,3 +172,30 @@ export function checkSizeOfStack(cards, stackSize) {
     return false;
   }
 }
+//Exercise: 6
+export function needsLicense(kind) {
+  if (kind === "car" || kind === "truck") {
+    return true;
+  } else {
+    return false;
+  }
+}
+needsLicense("car");
+
+export function chooseVehicle(option1, option2) {
+  return (
+    (option1 > option2 ? option2 : option1) + " is clearly the better choice."
+  );
+}
+chooseVehicle("Volkswagen Beetle", "Volkswagen Golf");
+
+export function calculateResellPrice(originalPrice, age) {
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age > 10) {
+    return originalPrice * 0.5;
+  } else if (age >= 3 && age <= 10) {
+    return originalPrice * 0.7;
+  }
+}
+calculateResellPrice(1000, 3);
