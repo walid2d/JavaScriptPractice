5bfffca0fd3159ab3d14fba2abebd8d978b74a02
Abdur R. Walid <ar_olid@outlook.com>
2022-09-02 23:17:09 -0600

    daily exercise

--- problemSolving.js
+++ problemSolving.js
@@ -590,3 +590,29 @@ function diagonalDifference(arr) {
 
   return Math.abs(left - right);
 }
+
+// Exercise: 18;
+
+export function getCardPosition(stack, card) {
+  return stack.indexOf(card);
+}
+
+export function doesStackIncludeCard(stack, card) {
+  return stack.includes(card);
+}
+
+export function isEachCardEven(stack) {
+  return stack.every((num) => num % 2 === 0);
+}
+
+export function doesStackIncludeOddCard(stack) {
+  return stack.some((num) => num % 2 !== 0);
+}
+
+export function getFirstOddCard(stack) {
+  return stack.find((num) => num % 2 !== 0);
+}
+
+export function getFirstEvenCardPosition(stack) {
+  return stack.findIndex((num) => num % 2 === 0);
+}
