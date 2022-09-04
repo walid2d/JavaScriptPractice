67a864d501d5c206dec33c93eb0da9297e65ec02
Abdur R. Walid <ar_olid@outlook.com>
2022-09-04 01:50:12 -0600

    daily exercise

--- problemSolving.js
+++ problemSolving.js
@@ -628,3 +628,63 @@ function staircase(n) {
     }
   }
 }
+export class Size {
+  constructor(width = 80, height = 60) {
+    this.width = width;
+    this.height = height;
+  }
+  resize(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+}
+export class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+}
+export class ProgramWindow {
+  constructor() {
+    this.screenSize = {
+      width: 800,
+      height: 600,
+    };
+    this.size = new Size();
+    this.position = new Position();
+  }
+
+  resize(newSize) {
+    if (newSize.width < 1) newSize.width = 1;
+    if (newSize.height < 1) newSize.height = 1;
+    if (newSize.width > this.screenSize.width)
+      newSize.width = this.screenSize.width - this.position.x;
+    if (newSize.height > this.screenSize.height)
+      newSize.height = this.screenSize.height - this.position.y;
+    this.size.height = newSize.height;
+    this.size.width = newSize.width;
+  }
+  move(newPosition) {
+    if (newPosition.x < 0) newPosition.x = 0;
+    if (newPosition.y < 0) newPosition.y = 0;
+    if (newPosition.x + this.size.width > this.screenSize.width)
+      newPosition.x = this.screenSize.width - this.size.width;
+    if (newPosition.y + this.size.height > this.screenSize.height)
+      newPosition.y = this.screenSize.height - this.size.height;
+    this.position.x = newPosition.x;
+    this.position.y = newPosition.y;
+  }
+}
+export function changeWindow(ins) {
+  const size = new Size(400, 300);
+  const position = new Position(100, 150);
+
+  ins.resize(size);
+  ins.move(position);
+  return ins;
+}
