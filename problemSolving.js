3e3aaff842210400679c6ed24843554a23317e33
Abdur R. Walid <ar_olid@outlook.com>
2022-09-05 02:20:02 -0600

    daily exercise

--- problemSolving.js
+++ problemSolving.js
@@ -688,3 +688,51 @@ export function changeWindow(ins) {
   ins.move(position);
   return ins;
 }
+//Exercise: 20;
+export class ArgumentError extends Error {}
+
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
+
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) {
+    throw new Error("Error");
+  }
+}
+
+export function reportOverheating(temperature) {
+  if (temperature === null) {
+    throw new ArgumentError();
+  }
+  if (temperature > 500) {
+    throw new OverheatingError(temperature);
+  }
+}
+
+export function monitorTheMachine(actions) {
+  try {
+    actions.check();
+  } catch (error) {
+    switch (error.constructor) {
+      case ArgumentError:
+        actions.alertDeadSensor();
+        return;
+      case OverheatingError: {
+        if (error.temperature > 600) {
+          actions.shutdown();
+          return;
+        }
+        if (error.temperature > 500) {
+          actions.alertOverheating();
+          return;
+        }
+      }
+      default:
+        throw error;
+    }
+  }
+}
