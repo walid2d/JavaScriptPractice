346501a6acba176b726dbf82d84f423d5e382356
Abdur R. Walid <ar_olid@outlook.com>
2022-09-19 23:35:26 -0600

    daily exercise

--- problemSolving.js
+++ problemSolving.js
@@ -1150,3 +1150,16 @@ export const parse = (string) => {
     .match(/\b[A-Z]/g)
     .join("");
 };
+function bSort() {
+  let v = prompt("Give me random numbers?");
+  let x = v.split(",");
+  for (let i = 0; i < x.length; i++) {
+    for (let y = 0; y < x.length; y++) {
+      if (x[y] * 1 > x[y + 1] * 1) {
+        let n = x[y] * 1;
+        x[y] = x[y + 1] * 1;
+        x[y + 1] = n;
+      }
+    }
+  }
+}
