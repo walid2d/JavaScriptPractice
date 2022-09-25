91d552c1f8b24da091333a370aeb7ed31c6c8157
Abdur R. Walid <ar_olid@outlook.com>
2022-09-25 00:07:21 -0600

    daily exercise hackerrank

--- problemSolving.js
+++ problemSolving.js
@@ -1300,3 +1300,16 @@ function getTotalX(a, b) {
   }
   return result.length;
 }
+function dayOfProgrammer(year) {
+  if (year >= 1700 && year <= 1918) {
+    if (year === 1918) return `26.09.${year}`;
+    if (year % 4 === 0) return `12.09.${year}`;
+    else return `13.09.${year}`;
+  } else {
+    if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+      return `12.09.${year}`;
+    } else {
+      return `13.09.${year}`;
+    }
+  }
+}
