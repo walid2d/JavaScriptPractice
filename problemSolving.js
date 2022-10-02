c21737761008876cbe354ba4974751615373bcc9
Abdur R. Walid <ar_olid@outlook.com>
2022-10-01 22:45:24 -0600

    daily exercise hackerrank

--- problemSolving.js
+++ problemSolving.js
@@ -1381,3 +1381,16 @@ export class List {
     return "UNEQUAL";
   }
 }
+export function transpose(input) {
+  if (input.length === 0) return [];
+  let maxLen = 0;
+  const array = input
+    .reverse()
+    .map((s) => {
+      maxLen = Math.max(maxLen, s.length);
+      return s + " ".repeat(maxLen - s.length);
+    })
+    .reverse()
+    .map((s) => [...s]);
+  return array[0].map((_, i) => array.map((r) => r[i])).map((r) => r.join(""));
+}
