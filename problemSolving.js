6cb1f6846a8dd1a062c2ed9c6e227ffbcb50438d
Abdur R. Walid <ar_olid@outlook.com>
2022-10-02 23:09:34 -0600

    daily exercise hackerrank

--- problemSolving.js
+++ problemSolving.js
@@ -1394,3 +1394,31 @@ export function transpose(input) {
     .map((s) => [...s]);
   return array[0].map((_, i) => array.map((r) => r[i])).map((r) => r.join(""));
 }
+const codons = {
+  Methionine: ["AUG"],
+  Phenylalanine: ["UUU", "UUC"],
+  Leucine: ["UUA", "UUG"],
+  Serine: ["UCU", "UCC", "UCA", "UCG"],
+  Tyrosine: ["UAU", "UAC"],
+  Cysteine: ["UGU", "UGC"],
+  Tryptophan: ["UGG"],
+  STOP: ["UAA", "UAG", "UGA"],
+};
+function find(codon) {
+  for (const name in codons) {
+    if (codons[name].includes(codon)) {
+      return name;
+    }
+  }
+  return false;
+}
+export function translate(RNA) {
+  if (!RNA) return [];
+  const names = RNA.match(/.{1,3}/g).map(find),
+    stop = names.findIndex((n) => n === "STOP"),
+    final = stop === -1 ? names : names.slice(0, stop);
+  if (final.some((n) => !n)) {
+    throw new Error("Invalid codon");
+  }
+  return final;
+}
